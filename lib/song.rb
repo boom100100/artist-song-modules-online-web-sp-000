8a0d72684986af57be46e20d761c340cadf7f6dd
Bernadette Davis <bsd231@nyu.edu>
2019-10-05 21:59:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,30 +12,8 @@ class Song
 
   @@songs = []
 
-
-=begin
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-=end
-def self.all
-    @@songs
-  end
-=begin
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-=end
   def artist=(artist)
     @artist = artist
   end
-=begin
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-=end
+  
 end
