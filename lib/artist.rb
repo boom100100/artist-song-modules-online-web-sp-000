a14a005088c0128ed1d726a22a35a7371ddc1e3e
Bernadette Davis <bsd231@nyu.edu>
2019-10-05 22:00:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,24 +16,10 @@ class Artist
     super
     @songs = []
   end
-=begin
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-=end
 
   def self.all
     @@artists
   end
-=begin
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-=end
 
   def add_song(song)
     @songs << song
@@ -43,9 +29,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-=begin
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-=end
+
 end
